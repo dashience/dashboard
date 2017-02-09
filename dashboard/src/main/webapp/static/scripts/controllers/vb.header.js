7c080353395c7abe0f6429d418fb9ded7d60657a
duc-dev-04 <duc-dev-04@duc-dev-04>
2017-02-09 13:29:06 +0530

    header bug fixing

--- dashboard/src/main/webapp/static/scripts/controllers/vb.header.js
+++ dashboard/src/main/webapp/static/scripts/controllers/vb.header.js
@@ -12,7 +12,7 @@ app.controller('HeaderController', function ($scope, $cookies, $http, $filter, $
     }
 
     $scope.selectDealer = {};
-    $http.get('admin/proxy/getJson?url=../dbApi/admin/dataSet/getData&connectionUrl=jdbc:mysql://localhost:3306/skyzone&startDate=09/07/2016&endDate=09/30/2016&username=root&password=root&port=3306&schema=richanalytics_prod&query=select location id, location dealerName from (select distinct location_1 location from Data) a').success(function (response) {
+    $http.get('admin/proxy/getJson?url=../dbApi/admin/dataSet/getData&connectionUrl=jdbc:mysql://localhost:3306/skyzone&startDate=09/07/2016&endDate=09/30/2016&username=root&password=&port=3306&schema=vb&query=select location id, location dealerName from (select distinct location_1 location from Data) a').success(function (response) {
         $scope.dealers = response.data;
 //        console.log($stateParams.locationId)
         $stateParams.locationId = $stateParams.locationId ? $stateParams.locationId : response.data[0].id;
