4d3c70f2c83fbd78fddb259b57066cb995d4f767
duc-dev-04 <duc-dev-04@duc-dev-04>
2017-04-05 16:13:22 +0530

    Merge origin/Sprint5-vs into Sprint5-Arul-Jose
    Conflicts:
    	dashboard/src/main/java/com/visumbu/vb/admin/controller/ProxyController.java

--- dashboard/src/main/webapp/static/scripts/controllers/vb.dataSource.js
+++ dashboard/src/main/webapp/static/scripts/controllers/vb.dataSource.js
@@ -273,17 +273,17 @@ app.directive('fileModel', ['$parse', function ($parse) {
         };
     }]);
 
-app.service('fileUpload', ['$http', function ($http) {
-        this.uploadFileToUrl = function (file, uploadUrl) {
-            var fd = new FormData();
-            fd.append('file', file);
-            $http.post(uploadUrl, fd, {
-                transformRequest: angular.identity,
-                headers: {'Content-Type': undefined}
-            })
-                    .success(function () {
-                    })
-                    .error(function () {
-                    });
-        };
-    }]);
+//app.service('fileUpload', ['$http', function ($http) {
+//        this.uploadFileToUrl = function (file, uploadUrl) {
+//            var fd = new FormData();
+//            fd.append('file', file);
+//            $http.post(uploadUrl, fd, {
+//                transformRequest: angular.identity,
+//                headers: {'Content-Type': undefined}
+//            })
+//                    .success(function () {
+//                    })
+//                    .error(function () {
+//                    });
+//        };
+//    }]);
