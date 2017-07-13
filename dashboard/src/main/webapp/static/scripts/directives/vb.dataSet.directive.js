4ce954043e302eb8b729f6061e93c48d286fe84c
arul <arul@dashience>
2017-07-13 11:56:30 +0530

    Fix Derived columns and duplicate data

--- dashboard/src/main/webapp/static/scripts/directives/vb.dataSet.directive.js
+++ dashboard/src/main/webapp/static/scripts/directives/vb.dataSet.directive.js
@@ -779,8 +779,8 @@ app.directive('previewTable', function ($http, $filter, $stateParams) {
                             // [0] the full match, [1] the first capture group => username
                             // Prepare the fake data
                             var listData = scope.tableColumns.filter(function (element) {
-                                return element.displayName.substr(0, match[1].length).toLowerCase() === match[1].toLowerCase()
-                                        && element.displayName.length > match[1].length;
+                                return element.fieldName.substr(0, match[1].length).toLowerCase() === match[1].toLowerCase()
+                                        && element.fieldName.length > match[1].length;
                             }).map(function (element) {
                                 return {
                                     display: element.displayName, // This gets displayed in the dropdown
