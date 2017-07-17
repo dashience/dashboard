1e3aee98be2205c20a6fe53cfb215a44d2e1f2fc
deldot <deldot@deldot-Aspire-E1-531>
2017-07-17 13:23:41 +0530

    chart color completed

--- dashboard/src/main/webapp/static/scripts/controllers/vb.pdf.js
+++ dashboard/src/main/webapp/static/scripts/controllers/vb.pdf.js
@@ -9,23 +9,36 @@ app.controller('PdfController', function ($stateParams, $http, $scope, $filter) 
             $scope.userAccountLogo = val.logo;
         });
     });
-    
+
     $http.get('admin/ui/dashboardTemplate/' + $stateParams.productId).success(function (response) {
-            $scope.templates = response;
-            var template = $filter('filter')(response, {id: $stateParams.templateId})[0];
-            $scope.templateName = template?template.templateName:null;
-        });    
-    
+        $scope.templates = response;
+        var template = $filter('filter')(response, {id: $stateParams.templateId})[0];
+        $scope.templateName = template ? template.templateName : null;
+    });
+
     $http.get("admin/ui/dbWidget/" + $stateParams.tabId + "/" + $stateParams.accountId).success(function (response) {
         var pdfProductName = response[0].tabId.agencyProductId ? response[0].tabId.agencyProductId.productName : null;
         $scope.userProductName = pdfProductName;
-        $scope.pdfWidgets = response;
+        var pdfWidgetItems = [];
+        pdfWidgetItems = response;
+        $http.get("admin/ui/getChartColorByUserId").success(function (response) {
+            var widgetColors;
+            if (response.optionValue) {
+                widgetColors = response.optionValue.split(',');
+            }
+            pdfWidgetItems.forEach(function (value, key) {
+                value.chartColors = widgetColors;
+            });
+            $scope.pdfWidgets = pdfWidgetItems;
+        }).error(function (response) {
+            $scope.pdfWidgets = pdfWidgetItems;
+        });
         setInterval(function () {
             window.status = "done";
-        }, 10000)
+        }, 10000);
     });
 
     $scope.downloadUiPdf = function () {
         window.open("admin/pdf/download?windowStatus=done&url=" + encodeURIComponent(window.location.href));
-    }
+    };
 });
