08e4931b8e4a54d68e79108ac395ea910956e572
deeta1 <deeta1@deeta1-Latitude-E6420>
2017-07-28 18:43:06 +0530

    Filter fixed

--- dashboard/src/main/java/com/visumbu/vb/admin/controller/ProxyController.java
+++ dashboard/src/main/java/com/visumbu/vb/admin/controller/ProxyController.java
@@ -692,7 +692,11 @@ public class ProxyController {
                     } else if (column.getFieldType() != null && column.getFieldType().equalsIgnoreCase("date") && column.getDataFormat() != null && !column.getDataFormat().isEmpty()) {
                         String value = data.get(column.getFieldName()) + "";
                         Date toDate = DateUtils.toDate(value, column.getDataFormat());
-                        data.put(column.getFieldName(), DateUtils.dateToString(toDate, "MM/dd/yyyy"));
+                        if (toDate == null) {
+                            data.put(column.getFieldName(), value);
+                        } else {
+                            data.put(column.getFieldName(), DateUtils.dateToString(toDate, "MM/dd/yyyy"));
+                        }
                         System.out.println("VALUE =============> " + value);
                     }
                 }
