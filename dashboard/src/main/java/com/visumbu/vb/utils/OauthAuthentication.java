26a62dd73f9bd6e16e10c54acd373b497450434a
sabari <sabari@sabari>
2017-12-08 19:03:57 +0530

    December 7th : ------------------ stanly code merged

--- dashboard/src/main/java/com/visumbu/vb/utils/OauthAuthentication.java
+++ dashboard/src/main/java/com/visumbu/vb/utils/OauthAuthentication.java
@@ -5,15 +5,11 @@
  */
 package com.visumbu.vb.utils;
 
-import static com.visumbu.vb.utils.JsonUtils.toList;
-import static com.visumbu.vb.utils.JsonUtils.toMap;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.security.GeneralSecurityException;
-import java.security.SecureRandom;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -21,8 +17,6 @@ import java.util.logging.Logger;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 /**
  *
@@ -131,16 +125,14 @@ public class OauthAuthentication {
             String oauth_signature = OauthAuthentication.computeSignature(signature_base_string, OauthAuthentication.encode((String) properties.get("consumerSecret")) + "&" + OauthAuthentication.encode((String) properties.get("tokenSecret")));
             String signature = OauthAuthentication.encode(oauth_signature);
             return signature;
-        } catch (GeneralSecurityException ex) {
-            Logger.getLogger(OauthAuthentication.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
+        } catch (GeneralSecurityException | UnsupportedEncodingException ex) {
             Logger.getLogger(OauthAuthentication.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
 
     public static Map<String, Object> setParameters(Map<String, Object> properties) {
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         for (Map.Entry<String, Object> entry : properties.entrySet()) {
             String key = entry.getKey();
             Object value = entry.getValue();
